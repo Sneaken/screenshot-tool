5c842915dd60f2e650ed9fd429efac4c58dcde53
Sneaken <924393527@qq.com>
2022-09-01 01:18:10 +0800

    feat: 新增系统托盘的菜单

--- src-tauri/src/system_tray.rs
+++ src-tauri/src/system_tray.rs
@@ -5,11 +5,15 @@ use tauri::SystemTray;
 pub fn init() -> SystemTray {
     let preferences = CustomMenuItem::new("preferences".to_string(), "偏好设置");
     let shot = CustomMenuItem::new("shot".to_string(), "截图");
+    let ding_from_clipboard = CustomMenuItem::new("ding_from_clipboard".to_string(), "从剪切板贴图");
+    let delayed_full_screenshot = CustomMenuItem::new("delayed_full_screenshot".to_string(), "延时全屏截图");
     let quit = CustomMenuItem::new("quit".to_string(), "退出");
     let tray_menu = SystemTrayMenu::new()
         .add_item(preferences)
         .add_native_item(SystemTrayMenuItem::Separator)
         .add_item(shot)
+        .add_item(ding_from_clipboard)
+        .add_item(delayed_full_screenshot)
         .add_native_item(SystemTrayMenuItem::Separator)
         .add_item(quit);
 
@@ -21,6 +25,8 @@ pub fn on_system_tray_event(_: &AppHandle, event: SystemTrayEvent) {
         SystemTrayEvent::MenuItemClick { id, .. } => {
             match id.as_str() {
                 "preferences" => {}
+                "ding_from_clipboard" => {}
+                "delayed_full_screenshot" => {}
                 "shot" => {}
                 "quit" => {
                     // 退出系统
